e97195b9f57ccdfa84d18fb5d25aadefeca1c4da
anatoly techtonik <techtonik@gmail.com>
2013-04-28 20:36:20 +0300

    hexdump() works for Python 2, release 0.1

--- hexdump.py
+++ hexdump.py
@@ -1,23 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: latin-1 -*-
-#
-# Dump binary data to the following text format:
-#
-# 0000000000: 00 00 00 5B 68 65 78 64  75 6D 70 5D 00 00 00 00  ...[hexdump]....
-# 0000000010: 00 11 22 33 44 55 66 77  88 99 AA BB CC DD EE FF  .."3DUfw........
-#
-# It is similar to the one used by:
-# Scapy
-# 00 00 00 5B 68 65 78 64 75 6D 70 5D 00 00 00 00  ...[hexdump]....
-# 00 11 22 33 44 55 66 77 88 99 AA BB CC DD EE FF  .."3DUfw........
-#
-# Far Manager
-# 000000000: 00 00 00 5B 68 65 78 64 ¦ 75 6D 70 5D 00 00 00 00     [hexdump]
-# 000000010: 00 11 22 33 44 55 66 77 ¦ 88 99 AA BB CC DD EE FF   ?"3DUfwˆ™ª»ÌÝîÿ
+"""
+Dump binary data to the following text format:
 
-__version__ = '0.0dev'
+0000000000: 00 00 00 5B 68 65 78 64  75 6D 70 5D 00 00 00 00  ...[hexdump]....
+0000000010: 00 11 22 33 44 55 66 77  88 99 AA BB CC DD EE FF  .."3DUfw........
+
+It is similar to the one used by:
+Scapy
+00 00 00 5B 68 65 78 64 75 6D 70 5D 00 00 00 00  ...[hexdump]....
+00 11 22 33 44 55 66 77 88 99 AA BB CC DD EE FF  .."3DUfw........
+
+Far Manager
+000000000: 00 00 00 5B 68 65 78 64 ¦ 75 6D 70 5D 00 00 00 00     [hexdump]
+000000010: 00 11 22 33 44 55 66 77 ¦ 88 99 AA BB CC DD EE FF   ?"3DUfwˆ™ª»ÌÝîÿ
+
+"""
+
+__version__ = '0.1'
 __author__  = 'anatoly techtonik <techtonik@gmail.com>'
 __license__ = 'Public Domain'
+
+__history__ = \
+"""
+0.1 (2013-04-28)
+ * working hexdump() function for Python 2
+"""
 
 import sys
 
@@ -75,11 +83,12 @@ def hexdump(data):
         line += '.'
     print(line)
 
+
 if __name__ == '__main__':
   hexdump('zzzz'*12)
   hexdump('o'*17)
   hexdump('p'*24)
   hexdump('q'*26)
-  hexdump('line\nfeed\ntest')
+  hexdump('line\nfeed\r\ntest')
   hexdump('\x00\x00\x00\x5B\x68\x65\x78\x64\x75\x6D\x70\x5D\x00\x00\x00\x00'
           '\x00\x11\x22\x33\x44\x55\x66\x77\x88\x99\xAA\xBB\xCC\xDD\xEE\xFF')
